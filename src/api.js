20c3a11757f111813c8f06ab444a48b0ed88dc90
Mariah Laqua <mariahlaqua@gmail.com>
2022-12-21 13:24:06 +0100

    Reworking the JSON parsing functions. Not complete.

--- src/api.js
+++ src/api.js
@@ -2,7 +2,15 @@ import axios from 'axios';
 
 const randomMealDb = async () => {
     const response = await axios.get('https://www.themealdb.com/api/json/v1/1/random.php');
-    return response.data.meals;
+    let ingredientArray = [];
+    let measurementArray = [];
+        Object.entries(response.data.meals[0]).forEach(([key, value]) =>
+            key.includes('strIngredient') && value ? ingredientArray.push(value) :null);
+        Object.entries(response.data.meals[0]).forEach(([key, value]) =>
+            key.includes('strMeasure') && value ? measurementArray.push(value) :null);
+        
+    
+    return ;
 
 }
 
